4aa458355959d6fde2216e2c573499b653edbce5
Fernando Sibarani <fernandosibarani45@gmail.com>
2024-08-24 16:39:13 +0700

    refactor: Update page layout and project component

--- src/presentation/layout/common-layout.tsx
+++ src/presentation/layout/common-layout.tsx
@@ -3,17 +3,13 @@ import Header from "@/presentation/components/header-component";
 
 export default function CommonLayout({
   children,
-  className = '',
 }: {
   readonly children: React.ReactNode;
-  readonly className?: string;
 }) {
   return (
     <>
       <Header />
-      <main className={`my-20 bg-white ${className}`}>
-        {children}
-      </main>
+      {children}
       <Footer />
     </>
   );
