0199e1df6a8f65e341e7c3100cf79adfbf11a497
martuafernando <fernandosibarani45@gmail.com>
2025-04-24 08:48:02 +0700

    chore: setup qwik

--- src/domain/entities/position.ts
+++
@@ -1,9 +0,0 @@
-export default interface Position {
-  readonly title: string;
-  readonly employmentType: string;
-  readonly location: string;
-  readonly locationType: string;
-  readonly startDate: string;
-  readonly endDate?: string;
-  readonly descriptionPoints: string[];
-}
