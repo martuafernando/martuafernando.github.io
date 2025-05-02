11b3463929af43feaf71675e1976ce7f173b06f4
martuafernando <fernandosibarani45@gmail.com>
2025-05-02 21:07:34 +0700

    feat: add experience page into home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,27 +2,21 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { HeroSection } from "~/components/sections/HeroSection";
 import { MarqueeSection } from "~/components/sections/MarqueeSection";
+import { getAllExperiences } from "~/repositories/experience";
 
 export default component$(() => {
+	const experiences = getAllExperiences()
+	const experienceImageList = experiences.map((experience) => ({
+		source: experience.companyLogoHorizontalUrl,
+		alt: experience.companyName,
+	}))
+
 	return (
 		<>
 			<HeroSection class="w-full h-[calc(100vh-96px)] relative z-10 bg-background rounded-b-[64px] sm:rounded-b-[96px] max-w-none" />
 			<MarqueeSection
 				class="w-full rounded-b-[64px] pt-40 pb-16 relative -top-24 sm:rounded-b-[96px] overflow-x-hidden bg-white max-w-none"
-				imageList={[
-					{
-						source: "https://placehold.co/600x400",
-						alt: "test",
-					},
-					{
-						source: "https://placehold.co/600x400",
-						alt: "test",
-					},
-					{
-						source: "https://placehold.co/600x400",
-						alt: "test",
-					},
-				]}
+				imageList={experienceImageList}
 			/>
 			<div class="h-[200vh]" />
 		</>
